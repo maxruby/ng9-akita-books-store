1eb0c520d65a31d834b24b14221c14ae47918192
Max Suster <mxsstr93@gmail.com>
2019-12-26 17:14:42 +0100

    [ADD] fixed imports and declarations

--- src/app/auth/containers/login-page/login-page.component.ts
+++ src/app/auth/containers/login-page/login-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { Creds } from '../state/auth.service';
-import { AuthService } from '../state/auth.service';
-import { Observable } from 'rxjs';
+import { Creds } from '../../state/auth.service';
+import { AuthService } from '../../state/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
 
   constructor(private authService: AuthService) { }
 
