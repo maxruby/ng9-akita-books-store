0e515856449db53ad121b7edf2a24960ab5210e0
Max Suster <mxsstr93@gmail.com>
2019-12-26 17:15:31 +0100

    [FIX] fixed declarations and imports

--- src/app/books/containers/collection-page/collection-page.component.ts
+++ src/app/books/containers/collection-page/collection-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Book } from '../state/book.model';
-import { BooksQuery } from '../state/books.query';
+import { Book } from '../../state/book.model';
+import { BooksQuery } from '../../state/books.query';
 
 @Component({
   selector: 'app-collection-page',
